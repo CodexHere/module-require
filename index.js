fde931a4df2a443078a5e8646183fb642d73738f
CodexHere <codexhere@outlook.com>
2023-10-20 02:32:18 -0500

    Delete cache first

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 console.log('This is the index file, every 3 seconds a module will reload');
 
 setInterval(() => {
+    const moduleCacheName = require.resolve('./myModule.js');
+    delete require.cache[moduleCacheName];
+
     const module = require('./myModule.js');
 
     module.exec();
